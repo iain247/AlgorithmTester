167536d1e5111bc151edc36f49fcffdd979b9af1
Iain Ross <iainross24.7@gmail.com>
2021-12-07 19:21:30 +0000

    cody tidying

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -62,6 +62,6 @@ namespace AlgorithmTester.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-     
+
     }
 }
---
+++ Controllers/InstructionsController.cs
@@ -0,0 +1,25 @@
+﻿using AlgorithmTester.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlgorithmTester.Controllers
+{
+    public class InstructionsController : Controller
+    {
+        private readonly ILogger<InstructionsController> _logger;
+
+        public InstructionsController(ILogger<InstructionsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Instructions()
+        {
+            return View();
+        }
+    }
+}
